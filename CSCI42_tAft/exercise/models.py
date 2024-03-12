f345f152807539250c88a82e583eddc48d69a80a
otiscow123 <elvis.tan@student.ateneo.edu>
2024-03-12 18:03:02 +0800

    created model for db that stores user exercises

--- CSCI42_tAft/exercise/models.py
+++ CSCI42_tAft/exercise/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 
-# Create your models here.
+# Exercise Model
 class ExerciseModel(models.Model):
     exercise_id = models.IntegerField(primary_key=True, null=False)
     exercise_name = models.CharField(max_length = 25)
-    duration = models.IntegerField()
     calories_burned_per_hour = models.IntegerField()
 
     def __str__(self):
             return self.exercise_id
-    
+
+ 
+#Exercise Done Model
+class ExerciseDone(models.Model):
+    user_exercise = models.IntegerField(primary_key=True, null=False)
+    model = models.ForeignKey(
+        'signup.UserModel',
+        on_delete=models.RESTRICT,
+        null=False)
+    model = models.ForeignKey(
+        ExerciseModel,
+        on_delete=models.RESTRICT,
+        null=False)
+    duration = models.IntegerField()
+    workout_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.user_exercise
