7357785652fa5a8f2fda2f3d1de11e527de104b1
Jan Enzo Salvador <jan.salvador@student.ateneo.edu>
2024-03-12 19:40:23 +0800

    Edited views of food app

--- CSCI42_tAft/food/views.py
+++ CSCI42_tAft/food/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 
-# Create your views here.
+from .models import *
+from .forms import *
+
+# Model Views
+class FoodDetailsView(DetailView):
+    model = FoodModel
+    template_name = 'food/food-details.html'
+
+
+class AddFoodView(CreateView):
+    model = FoodModel
+    form_class = FoodForm
+    template_name = 'food/food-add.html'
+
+
+class EditFoodView(UpdateView):
+    model = FoodModel
+    form_class = FoodForm
+    template_name = 'food/food-edit.html'
+
+def index(request):
+    foods = FoodModel.objects.all()
+    context = {
+        'foods': foods
+    }
+    return render(request, 'food/food.html', context)
