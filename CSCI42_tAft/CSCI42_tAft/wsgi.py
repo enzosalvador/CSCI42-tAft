e9fe21cf3c89f5c8ff22c75d0d0fc68e793be442
Jan Enzo Salvador <jan.salvador@student.ateneo.edu>
2024-03-12 15:10:59 +0800

    created the project CSCI42_tAft

---
+++ CSCI42_tAft/CSCI42_tAft/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for CSCI42_tAft project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CSCI42_tAft.settings')
+
+application = get_wsgi_application()
