092abdb3f64d3d2d7c8fb9ab8b11f2ce204cbe09
Jan Enzo Salvador <jan.salvador@student.ateneo.edu>
2024-03-12 19:04:38 +0800

    Registered UserModel to Admin

--- CSCI42_tAft/signup/admin.py
+++ CSCI42_tAft/signup/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    model = UserModel
+
+admin.site.register(UserModel, UserAdmin)
