e3e0f1789519b2adf2dd3e261551ad362e46cb44
otiscow123 <elvis.tan@student.ateneo.edu>
2024-03-12 17:49:26 +0800

    defined model for initial Food Database

--- CSCI42_tAft/food/models.py
+++ CSCI42_tAft/food/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db import reverse
 
-# Create your models here.
+
+#Food Model
+class FoodModel(models.Model):
+    food_id = models.IntegerField(primary_key=True, null=False)
+    food_name = models.CharField(max_length = 25)
+    calories_per_serving = models.IntegerField()
+    protein_per_serving = models.IntegerField()
+    fat_per_serving = models.IntegerField()
+    carbs_per_serving = models.IntegerField()
+
