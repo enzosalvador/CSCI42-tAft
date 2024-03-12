7fbe966e43cdbdd32af9a3b352b8ae36cfb7dc7d
Jan Enzo Salvador <jan.salvador@student.ateneo.edu>
2024-03-12 18:54:56 +0800

    Registered FoodModel to Admin

--- CSCI42_tAft/food/admin.py
+++ CSCI42_tAft/food/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class FoodAdmin(admin.ModelAdmin):
+    model = FoodModel
+
+admin.site.register(FoodModel, FoodAdmin)
