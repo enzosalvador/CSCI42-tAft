d0a307b3ef020decc9f371b752592815a1856f8c
Jan Enzo Salvador <jan.salvador@student.ateneo.edu>
2024-03-12 18:35:55 +0800

    Add the apps to the path

--- CSCI42_tAft/CSCI42_tAft/urls.py
+++ CSCI42_tAft/CSCI42_tAft/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("myactivity/", include('activity.urls', namespace="myactivity")),
+    path("food/", include('food.urls', namespace="food")),
+    path("exercise/", include('exercise.urls', namespace="exercise")),
+    path("login/", include('login.urls', namespace="login")),
+    path("signup/", include('signup.urls', namespace="signup")),
+    path("userprofile/", include('user.urls', namespace="userprofile")),
+    path("forum/", include('forum.urls', namespace="forum")),
 ]
