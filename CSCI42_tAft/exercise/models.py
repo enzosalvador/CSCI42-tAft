c901a3b0fbf89741b37944ceb78b07595f7fd942
otiscow123 <elvis.tan@student.ateneo.edu>
2024-03-12 17:58:14 +0800

    created model for db of exercises

--- CSCI42_tAft/exercise/models.py
+++ CSCI42_tAft/exercise/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ExerciseModel(models.Model):
+    exercise_id = models.IntegerField(primary_key=True, null=False)
+    exercise_name = models.CharField(max_length = 25)
+    duration = models.IntegerField()
+    calories_burned_per_hour = models.IntegerField()
+
+    def __str__(self):
+            return self.exercise_id
+    
