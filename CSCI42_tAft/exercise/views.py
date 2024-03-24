17a02ad069a7c0104a8a578235ad971ec7c29c77
Jan Enzo Salvador <jan.salvador@student.ateneo.edu>
2024-03-24 19:51:42 +0800

    Edited views of exercise

--- CSCI42_tAft/exercise/views.py
+++ CSCI42_tAft/exercise/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+
+from .models import *
+from .forms import *
 
 # Create your views here.
+class ExerciseDetailsView(DetailView):
+    model = ExerciseModel
+    template_name = 'exercise/exercise-details.html'
+
+class AddExerciseView(CreateView):
+    model = ExerciseModel
+    form_class = ExerciseModelForm
+    template_name = 'exercise/exercise-add.html'
+
+class EditExerciseView(UpdateView):
+    model = ExerciseModel
+    form_class = ExerciseModelForm
+    template_name = 'exercise/exercise-edit.html'
+
+def index(request):
+    exercises = ExerciseModel.objects.all()
+    context = {
+        'exercises': exercises
+    }
+    return render(request, 'exercise/exercise.html', context)
+
