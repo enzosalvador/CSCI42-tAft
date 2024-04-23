2cb2d3a1bea9b6beb21df4bf2572f422129e52fa
otiscow123 <elvis.tan@student.ateneo.edu>
2024-04-23 17:25:14 +0800

    updated admin.py

--- CSCI42_tAft/forum/admin.py
+++ CSCI42_tAft/forum/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ForumPost, Reply
 
-# Register your models here.
+class ReplyInLine(admin.TabularInline):
+    model = Reply
+
+# admin panel for Forum model
+class ForumPostAdmin(admin.ModelAdmin):
+    model = ForumPost
+    list_display = ("title", "author", "body", "pub_datetime")
+    inlines = [ReplyInLine]
+
+# admin panel for Reply model
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ("forum_post", "author", "body", "pub_datetime")
+
+
+admin.site.register(ForumPost, ForumPostAdmin)
+admin.site.register(Reply, ReplyAdmin)
