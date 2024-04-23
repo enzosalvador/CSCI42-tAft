a3aae88e055db1b8e8933cbe84f2bf15431d2db0
otiscow123 <elvis.tan@student.ateneo.edu>
2024-04-23 17:25:06 +0800

    created models for forum and reply

--- CSCI42_tAft/forum/models.py
+++ CSCI42_tAft/forum/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
+from userprofile.models import *
 
-# Create your models here.
+
+#Forum Model
+class ForumPost(models.Model):
+    title = models.CharField(max_length = 50)
+    body = models.TextField(max_length = 1000)
+    author = models.ForeignKey(UserProfileModel, on_delete = models.CASCADE)
+    pub_datetime = models.DateTimeField("Published Date and Time", auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('forum:forumpost_details', kwargs={'pk':self.pk})
+    
+# Reply
+# body; author; pub_datetime; forum_post;
+class Reply(models.Model):
+    body = models.TextField(max_length=1000)
+    author = models.ForeignKey(UserProfileModel, on_delete=models.CASCADE)
+    pub_datetime = models.DateTimeField("Published Date and Time", auto_now_add=True)
+    forum_post = models.ForeignKey(ForumPost, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body
+    
+    def get_absolute_url(self):
+        return reverse('forum:reply_details', kwargs={'post_pk': self.forum_post.pk, 'reply_pk': self.pk})
+
