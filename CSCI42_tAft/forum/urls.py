8937b1c6b6483c1953eae78084a164950cf6bac3
otiscow123 <elvis.tan@student.ateneo.edu>
2024-04-23 17:24:49 +0800

    configured urls.py

--- CSCI42_tAft/forum/urls.py
+++ CSCI42_tAft/forum/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import *
+from . import views
 
+# url for forum
 urlpatterns = [
-
+    path('', views.forum_view, name="forumposts"),
+    path('forumposts/add', views.ForumPostCreateView.as_view(), name="forumpost_add" ),
+    path('forumposts/<int:pk>/details', views.ForumPostDetailView.as_view(), name='forumpost_details'),
+    path('forumposts/<int:pk>/edit', views.ForumPostUpdateView.as_view(), name='forumpost_edit'),
+    path('forumposts/<int:post_pk>/reply/add', views.ReplyCreateView.as_view(), name="reply_add" ),
+    path('forumposts/<int:post_pk>/reply/<int:reply_pk>/details', views.ReplyDetailView.as_view(), name="reply_details" ),
+    path('forumposts/<int:post_pk>/reply/<int:reply_pk>/edit', views.ReplyUpdateView.as_view(), name="reply_edit" ),
 ]
+
 
 app_name = "forum"
